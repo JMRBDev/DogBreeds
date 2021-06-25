69de6d4143d986c3b06305b0fc94250f87b8276b
Jose Rosendo <josemrb99@gmail.com>
2021-06-25 04:12:53 +0200

    Add README.md images

--- src/enums/APP_STATES.js
+++
@@ -1,3 +0,0 @@
-export const READY = 'READY';
-export const LOADING = 'LOADING';
-export const ERROR_LOADING = 'ERROR_LOADING';
--- src/util/toUpperCase.js
+++
@@ -1,3 +0,0 @@
-const toUpperCase = (text) => text.slice(0, 1).toUpperCase() + text.slice(1);
-
-export default toUpperCase;
